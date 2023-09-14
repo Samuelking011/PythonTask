b0103b68b2115c4e8d7e5cca24ebb8d3cccf694d
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-09-14 11:42:52 -0700

    This is task on how to utilize Python

--- Task2.py
+++ Task2.py
@@ -1,18 +1,18 @@
 import os
 import git
 
-repo = git.Repo.clone_from("git@github.com:AmujoDotun/quiz.git","task2_clone", branch="master")
+#repo = git.Repo.clone_from("git@github.com:AmujoDotun/quiz.git","task2_clone", branch="master")
 
 if os.path.exists("task2_clone/src/main.js"):
     os.remove("task2_clone/src/main.js")
     print("main.js successfully removed")
 else:
-    print("main.js cannot be found")
+    print("main.js not found")
 
 if os.path.exists("task2_clone/src/App.vue"):
     os.remove("task2_clone/src/App.vue")
     print("App.vue successfully removed")
 else:
-    print("App.vue cannot be found")
+    print("App.vue not found")
 
 
