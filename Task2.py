14fa99ba81ea7c5d8ab02f5bd0806daed72c49dc
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-09-14 11:34:36 -0700

    This is continuation of Python assignment

---
+++ Task2.py
@@ -0,0 +1,18 @@
+import os
+import git
+
+repo = git.Repo.clone_from("git@github.com:AmujoDotun/quiz.git","task2_clone", branch="master")
+
+if os.path.exists("task2_clone/src/main.js"):
+    os.remove("task2_clone/src/main.js")
+    print("main.js successfully removed")
+else:
+    print("main.js cannot be found")
+
+if os.path.exists("task2_clone/src/App.vue"):
+    os.remove("task2_clone/src/App.vue")
+    print("App.vue successfully removed")
+else:
+    print("App.vue cannot be found")
+
+
